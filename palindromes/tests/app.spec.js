d5c8dee2dd97f51be5337d368338eda88e2a9762
Kayleigh Bellis <kayleigh@Kayleighs-MacBook-Pro.local>
2019-09-22 23:33:31 +0100

    Adding test for if string is not a palindrome

--- palindromes/tests/app.spec.js
+++ palindromes/tests/app.spec.js
@@ -33,10 +33,18 @@ describe('palindromes', () => {
       });
     });
 
-    it('it should return true if palindromes is a correct', () => {
-      chai.request(server).post('/palindrome').send("A man, a plan, a canal. Panama").end((err, res) => {
+    it('it should return true if string is a palindrome', () => {
+      chai.request(server).post('/palindrome').send("Damint I'm Mad").end((err, res) => {
         res.should.have.status(200);
-        res.body.should.be.eql(`'A man, a plan, a canal. Panama' : true`);
+        res.body.should.be.eql(`'Damint I'm Mad' : true`);
+        done();
+      });
+    });
+
+    it('it should return false if string is not a palindrome', () => {
+      chai.request(server).post('/palindrome').send("This is not a palindrome").end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.eql(`'This is not a palindrome' : true`);
         done();
       });
     });
