a3acd7412061ad6e30cea3672728ac6709f212a5
Kayleigh Bellis <kayleigh@Kayleighs-MacBook-Pro.local>
2019-09-24 11:47:09 +0100

    Updating test to remove dones

--- test/index-test.js
+++ test/index-test.js
@@ -10,7 +10,7 @@ describe('palindromes', () => {
 
   describe('/GET palindromes', () => {
     it('Should return 10 palindromes', () => {
-      chai.request(serverurl).get('/palindromes').end((err, res) => {
+      chai.request(serverurl).get('/palindromes').end(function (err, res) {
         expect(res.status).to.equal(200);
       });
     });
@@ -18,26 +18,25 @@ describe('palindromes', () => {
 
   describe('/POST palindromes', () => {
     it('it should not POST if palindromes is a number', () => {
-      chai.request(serverurl).post('/palindromes').send(6).catch((err) => {
+      chai.request(serverurl).post('/palindromes').send({"palindrome":6}).catch((err) => {
+        console.log(`res: `, res);
         expect(res.status).to.equal(400);
-      });
+      })
     });
 
     it('it should return true if string is a palindrome', () => {
-      chai.request(serverurl).post('/palindromes').send({"palindrome":"Dammit I'm Mad"}).end((err, res) => {
+      chai.request(serverurl).post('/palindromes').send({"palindrome":"Dammit I'm Mad"}).end( (err, res) => {
         expect(res.status).to.equal(200);
         expect(res.text).to.equal("true");
-      });
+      })
     });
 
     it('it should return false if string is not a palindrome', () => {
-      chai.request(serverurl).post('/palindromes').send({"palindrome":"This is not a palindrome"}).end((err, res) => {
+      chai.request(serverurl).post('/palindromes').send({"palindrome":"This is not a palindrome"}).end( (err, res) => {
         expect(res.status).to.equal(200);
         expect(res.text).to.equal("false");
-      });
+      })
     });
 
   });
-
-
 });
